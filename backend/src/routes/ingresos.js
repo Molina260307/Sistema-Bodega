52c3deaf02a48e234c8771a2b467ff45be25fe57
Molina260307 <133292423+Molina260307@users.noreply.github.com>
2026-07-23 19:52:39 -0600

    Fix ingresos.js (eliminado duplicado de express)

--- backend/src/routes/ingresos.js
+++ backend/src/routes/ingresos.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const path = require('path');
 const db = require('../db');
 
+
 // Configuración de almacenamiento
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
